95ab62e68fd5695b965202904f8b3607f021b733
repairkitchenappliance <repairkitchenappliance@gmail.com>
2023-02-12 01:26:51 +0500

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <Navbar />
       <Component {...pageProps} />
-      {/* <Analytics /> */}
+      <Analytics />
       <Calls />
       <Footer />
     </div>
