4f436777de507a9539f74ee29907bb774c480fef
repairkitchenappliance <repairkitchenappliance@gmail.com>
2023-03-08 23:31:18 +0500

    here

--- pages/index/PrivacyPolicy.js
+++ pages/index/PrivacyPolicy.js
@@ -14,16 +14,56 @@ const PrivacyPolicy = () => {
                     <div className="w-12 mr-1 h-0 border border-b-4 border-blue-500"></div>
                     <div className="w-2 h-0 border border-b-4 border-blue-500"></div>
                 </div>
-                <ul className='list-style'>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Personal Information Collection: Explain what types of personal information you collect from users, such as names, email addresses, phone numbers, and mailing addresses.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Use of Personal Information: Describe how you use the personal information you collect, such as to fulfill orders, process payments, or provide customer support.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Disclosure of Personal Information: Detail how and when you may disclose personal information to third parties, such as to shipping providers, payment processors, or law enforcement agencies.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Cookies and Tracking Technologies: Inform users if your website uses cookies or other tracking technologies and how these technologies may be used to collect and store personal information.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Security Measures: Describe the security measures you have in place to protect personal information from unauthorized access, use, or disclosure, such as encryption and firewalls.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>User Choices: Explain how users can exercise their choices regarding the collection, use, and disclosure of their personal information, such as opting out of email communications or requesting the deletion of their personal information.</li>
-                    <li className='bg-blue-500 p-2 cursor-pointer hover:bg-blue-600 rounded-md leading-none mt-1 text-white'>Compliance with Laws: State how you comply with relevant privacy laws, such as the General Data Protection Regulation (GDPR) or</li>
-                </ul>
+                <div className='list-style'>
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>This Privacy Policy governs the manner in which repairkitchenappliance.com collects, uses, maintains and discloses information collected from users of the repairkitchenappliance.com website. This privacy policy applies to the Site and all products and services offered by repairkitchenappliance.com.</div>
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">Personal identification information</b>
+                        <p className='mt-2'>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the siteplace an order and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, credit card information.</p>
+                    </div>
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">Users may, however, visit our Site anonymously</b>
+                        <p className='mt-2'>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+                    </div>
+
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">Web browser cookies</b>
+                        <p className='mt-2'>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+                    </div>
+
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">How we use collected information</b>
+                        <p className='mt-2'>repairkitchenappliance.com collects and uses Users personal information for the following purposes:
+                            To improve our Site</p>
+                        <p className='mt-2'>We continually strive to improve our website offerings based on the information and feedback we receive from you.
+                            To process transactions</p>
+                        <p className='mt-2'>We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.</p>
+                        <p className='mt-2'>To administer a content, promotion, survey or other Site feature</p>
+                        <p className='mt-2'>To send Users information they agreed to receive about topics we think will be of interest to them.
+                            To send periodic emails</p>
+                    </div>
+                    <div>
+                        <b className="">How we protect your information</b>
+                        <p className='mt-2'>
+                            We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.
+                        </p>
+                        <p className='mt-2'>
+                            By using this Site, you signify your acceptance of this policy and terms of service. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.
+                        </p>
+                    </div>
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">Users may, however, visit our Site anonymously</b>
+                        <p className='mt-2'>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+                    </div>
+
+                    <div className='bg-white shadow-lg p-4 cursor-pointer hover:bg-gray-200 rounded-md leading-none mt-2 text-gray-700'>
+                        <b className="">Users may, however, visit our Site anonymously</b>
+                        <p className='mt-2'>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+                    </div>
+
+                </div>
             </div>
+
+
         </div>
     )
 }
